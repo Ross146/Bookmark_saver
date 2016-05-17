d806d691f52ed611ccf44d3a0a88ef6e876ee6b9
Ross146 <padalk46@gmail.com>
2016-05-18 00:08:28 +0300

    initialize application change

--- Bookmark_saver(no requirejs)/js/app.js
+++ Bookmark_saver(no requirejs)/js/app.js
@@ -1,41 +1,29 @@
 var app = app || {};
+
+var application = Marionette.Application.extend({
+    initialize: function () {
+        console.log("initialize application");
+        this.applicationView = new app.AppView();
+        this.mainRegion.show(this.applicationView);
+        this.router = new window.app.Router(this);
+        this.on('start', function () {
+            console.log('start BackboneHistory');
+            Backbone.history.start();
+        });
+    },
+    regions: {
+        'mainRegion': 'body'
+    }
+
+});
+
 (function () {
 
-    var application = Marionette.Application.extend({
-        initialize: function () {
-            console.log("initialize application");
-        }
-    });
     window.app1 = new application;
-    app1.addRegions({
-        'mainRegion': 'body'
-    });
-    app1.applicationView = new app.AppView();
-    app1.mainRegion.show(app1.applicationView);
-
-    app1.router = new window.app.Router(app1); //передавать app1 ???
-    app1.on('start', function () {
-        console.log('start BackboneHistory');
-        Backbone.history.start();
-    })
-
     app1.start();
 
 }) ();
 
-var bookmark1 = new app.Bookmark({
-    title: "habra",
-    description: "новости",
-    location: "habrahabr.ru",
-    priority: 5
-});
-
-var bookmark2 = new app.Bookmark({
-    title: "adfsadf",
-    description: "asdfasdf",
-    location: "asdfasdf",
-    priority: 6
-});
 
 
 
