9e9785e1fdfa6df31e141fc70b46800288ef8d85
padalka <y.padalka@umberfirm.com>
2016-05-19 19:51:42 +0300

    mainRegion initialize

--- Bookmark_saver(no requirejs)/js/app.js
+++ Bookmark_saver(no requirejs)/js/app.js
@@ -1,25 +1,23 @@
 var app = app || {};
 
 var application = Marionette.Application.extend({
-    initialize: function () {
+    initialize: function (options) {
         console.log("initialize application");
         this.applicationView = new app.AppView();
+        this.addRegions({mainRegion : 'body'});
         this.mainRegion.show(this.applicationView);
         this.router = new window.app.Router(this);
         this.on('start', function () {
             console.log('start BackboneHistory');
             Backbone.history.start();
         });
-    },
-    regions: {
-        'mainRegion': 'body'
     }
 
 });
 
 (function () {
 
-    window.app1 = new application;
+    window.app1 = new application({container: 'body'});
     app1.start();
 
 }) ();
