b6465be536d776da64893352d81fe5714406466f
Ross146 <padalk46@gmail.com>
2016-05-20 00:33:54 +0300

    last

--- Bookmark_saver(no requirejs)/js/views/appView.js
+++ Bookmark_saver(no requirejs)/js/views/appView.js
@@ -1,41 +1,32 @@
 var app = app || {};
 
-(function () {
-    app.AppView = Marionette.LayoutView.extend({
-        template: _.template($('#maincontent').text()),
+app.AppView = Marionette.LayoutView.extend({
+    template: _.template($('#maincontent').text()),
 
-        initialize: function () {
-            console.log("appView initialize");
-        },
+    initialize: function () {
+        console.log("appView initialize");
+    },
 
-        regions : {
-            container: '#content',
-            header: '#header',
-            footer: '.footer'
-        },
+    regions: {
+        container: '#content',
+        header: '#header',
+        footer: '.footer'
+    },
 
-        onRender:function(){
-            this.renderHeader();
-            this.footer.show(new app.FooterView());
-        },
+    onRender: function () {
+        this.header.show(new app.HeaderView());
+        this.footer.show(new app.FooterView());
+    },
 
-        renderHeader: function() {
-            this.header.show(new app.HeaderView());
-        },
-
-        showContent:function(view){
-            if (view){
-                this.contentView = view;
-            }
-            this.container.show(this.contentView);
-        },
-
-        refreshContent: function(view){
-            this.showContent(view);
+    showContent: function (view) {
+        if (view) {
+            this.contentView = view;
         }
+        this.container.show(this.contentView);
+    },
 
-    });
+    refreshContent: function (view) {
+        this.showContent(view);
+    }
 
-
-
-})()
+});
